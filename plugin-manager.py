ff54969ce9d2c7004994c177dba2d335d2302b7b
XxKingsxX-Pinu <Kieran@digitalrealism.com.au>
2024-03-04 08:03:50 +1000

    Update plugin-manager.py

--- plugin-manager.py
+++ plugin-manager.py
@@ -1,1 +1,66 @@
 
+import pwnagotchi.plugins as plugins
+from flask import abort
+from flask import render_template_string
+import subprocess
+
+import time
+import json
+import logging
+
+import requests
+
+class pluginManager(plugins.Plugin):
+    __author__ = "discord@rai68"
+    __version__ = "1.3.0"
+    __license__ = "LGPL"
+    __description__ = "Provides an easy gateway to access plugins"
+
+    def __init__(self):
+        #each time load
+        self.loaded = False
+        self.ready = False
+        self.accessToken = None
+        
+        
+        #first load, changes on each time load.
+        self.db = None
+    
+        
+        #frequently updated
+        self.internet = False
+        
+        
+    def on_loaded(self):
+        logging.info("[Plugin Manager] plugin loading begin")
+        
+
+
+
+      
+    def on_webhook(self, path, request):
+        if self.ready and self.loaded is False:
+            return "<html><head><title>Plugin Manager: Error</title></head><body><code>%s</code></body></html>" % "Plugin is still booting. Please try again soon"
+        if self.internet is False:
+            return "<html><head><title>Plugin Manager: Error</title></head><body><code>%s</code></body></html>" % "Internet is not connected. Please try again soon"
+        
+        
+        
+        if request.method == "GET":
+            #all gets below
+            try:
+                logging.debug(path)
+                if path is None:
+                    #GET "/" 
+                    mainHTML = []
+                    
+                    
+                    
+                    return mainHTML
+                elif path == '/plugin/':
+                    #GET "/plugin/PLUGINNAME" - will open a menu with plugin info and versioning
+                    pluginName = path.split("/plugin/")[1]
+
+          
+            except Exception as e:
+                return "<html><head><title>Plugin Manager: Error</title></head><body><code>%s</code></body></html>" % e
